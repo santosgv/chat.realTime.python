e69acbdb8d10a62b2742c40877afe0e4b5d27bbf
Vitor.Gomes <santosgomesv@gmail.com>
2022-01-27 11:37:30 -0300

    Scroll bar lateral

--- cliente.py
+++ cliente.py
@@ -26,15 +26,20 @@ class Chat:
         self.root =Tk()
         self.root.geometry('800x800')
         self.root.title('Chat')
-        self.caixa_texto=Text(self.root)
+        self.caixa_texto=Text(self.root,background="black", foreground="green")
         self.caixa_texto.place(relx=0.05, rely=0.08, width=700,height=600)
+
+        self.scroller = Scrollbar(self.root)
+        self.scroller.place(relx=0.900, rely=0.08, width=20,height=600)
+        self.scroller.config(command=self.caixa_texto.yview)
+        self.caixa_texto.config(yscrollcommand = self.scroller.set)
 
         self.envia_mensagem= Entry(self.root)
         self.envia_mensagem.place(relx=0.05, rely=0.9, width=500,height=20)
 
         self.btn_envia =Button(self.root,text='Envia',command=self.enviarMensagem)
         self.btn_envia.place(relx=0.7, rely=0.9, width=100,height=20)
-        self.root.protocol('WM_DELETE_WINDOW',self.fechar)
+        self.root.protocol(self.fechar)
 
         self.root.mainloop()
 
